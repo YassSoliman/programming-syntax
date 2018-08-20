495165c00aa572d8f25f810e11a025d58ed8324b
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-08-20 15:35:43 -0400

    Locally saving all the data using a script

--- app.js
+++ app.js
@@ -4,14 +4,48 @@ var http = require('http').Server(app);
 var request = require('request');
 var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
+var fs = require('fs');
 const PORT = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Commented because only need to run it when there is an update in the original website
+// function createBackups(){
+//     request('http://rigaux.org/language-study/syntax-across-languages-per-language/', function(err, response, body){
+//         if(!err && response.statusCode == 200){
+//             var titles = parseTitles(body);
+//             console.log(titles);
+//             for(var i=0; i<titles.length-4; i++){
+//                 var currentTitle = titles[i];
+//                 getRessource(currentTitle);
+//             }
+//         }
+//     });
+// }
+// 
+// function getRessource(title){
+//     request('http://rigaux.org/language-study/syntax-across-languages-per-language/'+title+'.html', function(e, r, b){
+//         if(!e && r.statusCode == 200){
+//             
+//             var newBody = parseData(b);
+//             fs.writeFile('data/'+title+'.html', newBody, function(err){
+//                if(err) throw err;
+//                 console.log(title + ' saved');
+//             });
+//         }
+//     });
+// 
+// }
 
 app.get("/", function(req, res){
-	res.render('index');
+    createBackups();
+    request('http://rigaux.org/language-study/syntax-across-languages-per-language/', function(err, response, body){
+        if(!err && res.statusCode == 200){
+            var titles = parseTitles(body);
+            res.render('index', {titles: titles});
+        }
+    });
 });
 
 app.get("/api", function(req, res){
@@ -32,7 +66,16 @@ function parseData(data){
     const $ = cheerio.load(data);
     
     return ($('ul:nth-of-type(2)').html());
+}
 
+function parseTitles(data){
+    const $ = cheerio.load(data);
+    var titles = [];
+    $('li a').each(function(){
+        titles.push($(this).html());
+    });
+
+    return titles;
 }
 
 http.listen(PORT, function () {
