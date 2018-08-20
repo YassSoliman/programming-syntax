936c68d8ebe9399a2dfc521fc7078ededb292b44
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-08-20 16:28:20 -0400

    Removed spacing from data files

--- app.js
+++ app.js
@@ -11,34 +11,35 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // Commented because only need to run it when there is an update in the original website
-// function createBackups(){
-//     request('http://rigaux.org/language-study/syntax-across-languages-per-language/', function(err, response, body){
-//         if(!err && response.statusCode == 200){
-//             var titles = parseTitles(body);
-//             console.log(titles);
-//             for(var i=0; i<titles.length-4; i++){
-//                 var currentTitle = titles[i];
-//                 getRessource(currentTitle);
-//             }
-//         }
-//     });
-// }
-// 
-// function getRessource(title){
-//     request('http://rigaux.org/language-study/syntax-across-languages-per-language/'+title+'.html', function(e, r, b){
-//         if(!e && r.statusCode == 200){
-//             
-//             var newBody = parseData(b);
-//             fs.writeFile('data/'+title+'.html', b, function(err){
-//                if(err) throw err;
-//                 console.log(title + ' saved');
-//             });
-//         }
-//     });
-// 
-// }
+//  function createBackups(){
+//      request('http://rigaux.org/language-study/syntax-across-languages-per-language/', function(err, response, body){
+//          if(!err && response.statusCode == 200){
+//              var titles = parseTitles(body);
+//              console.log(titles);
+//              for(var i=0; i<titles.length-4; i++){
+//                  var currentTitle = titles[i];
+//                  getRessource(currentTitle);
+//              }
+//          }
+//      });
+//  }
+//  
+//  function getRessource(title){
+//      request('http://rigaux.org/language-study/syntax-across-languages-per-language/'+title+'.html', function(e, r, b){
+//          if(!e && r.statusCode == 200){
+//              
+//              var newBody = parseData(b);
+//              fs.writeFile('data/'+title+'.html', b, function(err){
+//                 if(err) throw err;
+//                  console.log(title + ' saved');
+//              });
+//          }
+//      });
+//  
+//  }
 
 app.get("/", function(req, res){
+    createBackups();
     fs.readFile('data/languages.html', 'utf8', function(err, body){
         if(!err){
             var titles = parseTitles(body);
@@ -71,7 +72,9 @@ function parseTitles(data){
     const $ = cheerio.load(data);
     var titles = [];
     $('li a').each(function(){
-        titles.push($(this).html());
+        var str = $(this).html();
+        str = str.replace(/\s/g, '');
+        titles.push(str);
     });
 
     return titles;
