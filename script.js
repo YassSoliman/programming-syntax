3c9122fd5acfc24c24e16b689d95f92857465d5f
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-08-20 11:15:23 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var requestURL = 'https://rigaux.org/language-study/syntax-across-languages.html';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'document';
+request.send();
+request.onload = function(){
+    console.log(request);
+    /*
+    // Before this step need to get the website http://rigaux.org/language-study/syntax-across-languages/Vrs.html 
+    var pythonSyntax = document.evaluate('//tr[contains(., "Python")]', document, null, XPathResult.ANY_TYPE, null);
+    var titles = document.evaluate('//li[contains(., "Python")]', document, null, XPathResult.ANY_TYPE, null);
+    
+    var iterator = pythonSyntax.iterateNext();
+    var titleIterator = titles.iterateNext();
+    
+    var alertText = 'Python syntax is:\n'
+    var syntax = {
+        python: []
+    };
+    // Will output all the syntax part of the website related to python
+    while(iterator){
+            alertText += iterator.querySelector('td').textContent + '\n';
+            syntax.python.push(titleIterator.querySelector('a').textContent+' - ' + iterator.querySelector('td').textContent);
+            console.log(alertText);
+            iterator = pythonSyntax.iterateNext();
+            titleIterator = titles.iterateNext();
+    }
+    */
+}
